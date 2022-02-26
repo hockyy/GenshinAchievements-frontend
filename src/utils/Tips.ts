6fbac04ccd1b46e02b3afa47ab394d5afba02068
Hocky Yudhiono <hocky.yudhiono@gmail.com>
2022-02-26 16:28:52 +0700

    Add tips

--- src/utils/Tips.ts
+++ src/utils/Tips.ts
@@ -3,6 +3,6 @@ export const Tips = [
   'Klik kanan dan pilih "Simpan Gambar Sebagai..." untuk menyimpan gambar <i>achievement</i>.',
   'Kamu bisa menggunakan hiragana juga, misalnya "ありがとう".',
   'Sebenarnya ini servernya menggunakan <i>Education Plan</i>.',
-  'Dari riset dan pengolahan citra, situs ini dibuat dalam waktu kurang dari 17 jam.',
+  'Dari riset dan pengolahan citra (termasuk tidur 😴 7 jam), situs ini dibuat dalam waktu kurang dari 17 jam.',
   'Server <i>backend API</i> dari aplikasi ini tidak diberi perlindungan batas banyak permintaan.',
 ];
